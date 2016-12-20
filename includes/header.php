005040ef1fad5308a03565de98ec257e4be235c5
Itanortega <christian.segundo@hotmail.com>
2016-12-19 20:15:25 -0500

    Menu completo

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,17 @@
+<div class="container-fluid barra_negra">
+	<div class="container">
+		<div class="row top-marg">
+		  <div class="col-12">
+		  	<div class="pull-right">
+		  		<a target="_blank" class="iconos-sociales" href="https://www.facebook.com/Servitramites-Exprees-256915627738258/"><span class="fa fa-facebook iconos">&nbsp;&nbsp;</span></a>
+                |
+                <a target="_blank" class="alogin" href="https://mail.google.com/mail/u/0/#inbox"><span class="fa fa-envelope"><span class="login">&nbsp;Correo Empresarial&nbsp;</span></span></a>
+                |
+                <a target="_blank" class="alogin" href="http://servitramitesexpress.net/steweb/steweb.html"><span class="fa fa-sign-in"><span class="login">&nbsp;Login&nbsp;</span></span></a>					  	</div>
+		  </div>
+		</div>
+	</div>
+</div>
 <!-- Navigation -->
     <nav id="mainNav" class="navbar navbar-default navbar-custom">
         <div class="container">
@@ -5,7 +19,9 @@
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span> Menu &nbsp;<i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <img src="img/logo1.png" alt="">
+                </a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -14,19 +30,36 @@
                         <a href="#page-top"></a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#services">Services</a>
+                        <a class="menuitem <?php echo $active_home; ?>" href="index.php?page=home">
+                            <div>
+                                <div class="iconmenuitem hidden-xs"><center><span class="fa fa-home" aria-hidden="true"></span></center></div>
+                                <div>INICIO</div>
+                            </div>
+                        </a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#portfolio">Portfolio</a>
+                        <a class="menuitem <?php echo $active_about; ?>" href="index.php?page=about">
+                            <div>
+                                <div class="iconmenuitem hidden-xs"><center><span class="fa fa-user-circle" aria-hidden="true"></span></center></div>
+                                <div>NOSOTROS</div>
+                            </div>
+                        </a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#about">About</a>
+                        <a class="menuitem <?php echo $active_service; ?>" href="index.php?page=service">
+                            <div>
+                                <div class="iconmenuitem hidden-xs"><center><span class="fa fa-bars" aria-hidden="true"></span></center></div>
+                                <div>SERVICIOS</div>
+                            </div>
+                        </a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#team">Team</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Contact</a>
+                        <a class="menuitem <?php echo $active_contact; ?>" href="index.php?page=contact">
+                            <div>
+                                <div class="iconmenuitem hidden-xs"><center><span class="fa fa-comments" aria-hidden="true"></span></center></div>
+                                <div>CONTACTO</div>
+                            </div>
+                        </a>
                     </li>
                 </ul>
             </div>
--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <?php
 if(empty($_GET['page'])) {
-  $pagina='home.php';
+    $pagina='home.php';
+    $active_home = 'menu-item-active';
+    $active_about = '';
+    $active_service = '';
+    $active_contact = '';
 }else {
+    $active_home = '';
+    $active_about = '';
+    $active_service = '';
+    $active_contact = '';
     switch ($_GET['page']) {
         case 'home':
             $pagina = 'home.php';
+            $active_home = 'menu-item-active';
+            break;
+
+        case 'about':
+            $pagina = 'about.php';
+            $active_about = 'menu-item-active';
+            break;
+
+        case 'service':
+            $pagina = 'service.php';
+            $active_service = 'menu-item-active';
+            break;
+
+        case 'contact':
+            $pagina = 'contact.php';
+            $active_contact = 'menu-item-active';
             break;
 
         default:
             $pagina = 'home.php';
+            $active_home = 'menu-item-active';
             break;
     }
 }
