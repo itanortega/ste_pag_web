aedaddad8cc43326fe15279d8cee33c3f1c8908d
Itanortega <christian.segundo@hotmail.com>
2016-12-22 15:07:43 -0500

    Creación del footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,26 @@
+        <footer>
+			<div class="container-fluid">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-md-4 widget">
+							<h3>Siguenos:</h3><ul><h5><a target="_blank" class="iconos-sociales2" href="https://www.facebook.com/Servitramites-Exprees-256915627738258/"><span class="fa fa-facebook iconos">&nbsp;<span class="redes-sociales-footer">Facebook&nbsp;</span></span></a></h5></ul>					  		<br>
+					  		<h3>Contactos:</h3><ul><h5><span class="fa fa-road iconos">&nbsp;Calle 12 No. 20-06 Av. Boyaca Pasto Nariño Colombia</span></h5><h5><span class="fa fa-phone iconos">&nbsp;722 7472  - Fax: 729 3998</span></h5><h5><span class="fa fa-mobile iconos">&nbsp;315 483 3830</span></h5><h5><span class="fa fa-envelope iconos">&nbsp;Info@servitramitesExpress.net</span></h5></ul>
+						</div>
+						<div class="col-xs-12 col-md-8 widget">
+						<br>
+							<iframe src="https://www.google.com/maps/d/embed?mid=zFDWK2fQUrk4.kKnfjXCbWqWw&amp;z=15" width="100%" height="300"></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="container mg">
+						<div class="pull-right"><br>© Servitramites Express 2016, Todos los derechos reservados.</div>
+				</div>
+			</div>
+		</footer>
+
+
+
+
         <div class="scroll-top-wrapper ">
             <span class="scroll-top-inner">
                 <i class="fa fa-2x fa-arrow-circle-up"></i>
